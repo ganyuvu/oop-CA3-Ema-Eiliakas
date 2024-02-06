01eda66af6b07e03c5fd17ada2934f71dd2143b4
ganyuvu <eeiliakas@gmail.com>
2024-02-06 17:13:33 +0000

    Started basics for Q5

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,6 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
 /**
  *  Name:
- *  Class Group:
+ *  Class Group: GD2a
  */
 
 public class CA3_Question5
@@ -8,6 +13,28 @@ public class CA3_Question5
 
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
+        Queue<String> landings = new LinkedList<>();
+        Queue<String> takeOffs = new LinkedList<>();
+
+        System.out.println("Commands list: ");
+        System.out.println("[takeOff <flightSymbol>] to queue plane");
+        System.out.println("[land <flightSymbol>] to queue plane");
+        System.out.println("[Next] to complete takeoff/landing ");
+        System.out.println("[Quit] to finish the simulation");
+
+        String userInput = scanner.nextLine().toLowerCase();
+
+        if (userInput.contains("takeoff"))
+        {
+            String flightSymbol = userInput.substring(7).trim();
+            takeOffs.add(flightSymbol);
+        }
+        else if(userInput.contains("land"))
+        {
+            String flightSymbol = userInput.substring(4).trim();
+            landings.add(flightSymbol);
+        }
 
     }
 }
