818be510410350c6eb6560f57269f01bace876cc
ganyuvu <eeiliakas@gmail.com>
2024-02-11 22:23:46 +0000

    Started Q10 still trying to figure it out, created a txt file for it

--- src/CA3_Question10.java
+++ src/CA3_Question10.java
@@ -1,11 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 /**
- *  Name:
- *  Class Group:
+ *  Name: Ema Eiliakas
+ *  Class Group: GD2a
  */
 public class CA3_Question10
 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, TreeSet<DistanceTo>> map = new HashMap<>();
+        HashMap<String, HashSet<DistanceTo>> distances = new HashMap<>();
+        PriorityQueue<DistanceTo> priorityQueue = new PriorityQueue<>();
 
-    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new File("cities.txt"));
+        String startingCity = scanner.next(); //this retrieves the first city from the file
+
+        // adding cities + distances from txt file to the map
+        while (scanner.hasNext()) {
+            String city1 = scanner.next();
+            String city2 = scanner.next();
+            int distance = scanner.nextInt();
+
+            if (!distances.containsKey(city1)) {
+                distances.put(city1, new HashSet<>());
+            }
+            distances.get(city1).add(new DistanceTo(city2, distance));
+        }
+        //starting the queue with the first city in the file
+        priorityQueue.offer(new DistanceTo(startingCity, 0));
 
     }
 }
